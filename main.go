a7b390d30f1a5887af8a46814b42065dce07a8cb
Loki Verloren <l0k1@null.net>
2018-08-25 12:58:26 +0200

    playing with multiple push URLs

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	_ "github.com/awnumar/memguard"
-	_ "gitlab.com/parallelcoin/duo/pkg/types"
+	"fmt"
+	"gitlab.com/parallelcoin/duo/pkg/types"
 )
 
 func main() {
-
+	testthing := types.NewBytes()
+	fmt.Println(testthing)
 }
