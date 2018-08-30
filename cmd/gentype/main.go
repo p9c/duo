a05f6a1346b9ec8ea4bc61b69bea16b282ddf6e5
Loki Verloren <l0k1@null.net>
2018-08-30 05:52:58 +0200

    writing a tool to generate types from interface spec

--- cmd/gentype/main.go
+++ cmd/gentype/main.go
@@ -2,75 +2,140 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"strings"
 )
 
-var S skeleton
+var (
+	lalpha = "abcdefghijklmnopqrstuvwxyz"
+	ualpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	number = "0123456789"
+	symbol = `()[]{}"\/'*.,;:`
+	whole  = `package testprototype
 
-type skeleton struct {
-	pkg   string
-	imp   []string
-	name  string
-	iface string
-	vals  [][]string
-	meths [][]string
+	import (
+		. "gitlab.com/parallelcoin/duo/pkg/bytes"
+	)
+	
+	type TestPrototype struct {
+		val  *[]byte
+		set  bool
+		utf8 bool
+		err  error
+	}
+	
+	type testPrototype interface {
+		Buf() []byte
+		Copy(*Bytes) *Bytes
+		Delete()
+		Error() string
+		IsSet() bool
+		IsUTF8() bool
+		Len() int
+		Link(*Bytes) *Bytes
+		Load(*[]byte) *Bytes
+		MarshalJSON() ([]byte, error)
+		Move(*Bytes) *Bytes
+		New(int) *Bytes
+		Null() *Bytes
+		Rand(int) *Bytes
+		SetBin() *Bytes
+		SetError(string) *Bytes
+		SetUTF8() *Bytes
+		String() string
+	}`
+)
+
+type Lex struct {
+	symtype string
+	content string
+}
+
+func isWhiteSpace(b byte) bool {
+	switch b {
+	case ' ', '\n', '\t':
+		return true
+	}
+	return false
+}
+
+func isSymbol(b byte) bool {
+	for i := range symbol {
+		if b == symbol[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func isAlpha(b byte) bool {
+	for i := range lalpha {
+		if b == lalpha[i] {
+			return true
+		}
+	}
+	for i := range ualpha {
+		if b == ualpha[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func isNum(b byte) bool {
+	for i := range number {
+		if b == number[i] {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
-	text, _ := ioutil.ReadAll(os.Stdin)
-	words := strings.Fields(string(text))
-	var i int
+	lexed := [][]byte{}
+	// whole, _ := ioutil.ReadAll(os.Stdin)
+	i := 0
 	for {
-		if words[i] == "package" {
+		b := []byte{}
+		switch {
+		case isWhiteSpace(whole[i]):
 			i++
-			S.pkg = words[i]
+		case isAlpha(whole[i]):
+			b = append(b, whole[i])
 			i++
-		}
-		if words[i] == "import" {
-			i++
-			if words[i] == "(" {
-				i++
-				for words[i] != ")" {
-					S.imp = append(S.imp, words[i])
-					i++
-				}
+			for isAlpha(whole[i]) || isNum(whole[i]) {
+				b = append(b, whole[i])
 				i++
 			}
-			S.imp = append(S.imp, words[i])
-		}
-		if words[i] == "type" {
+			lexed = append(lexed, b)
+		case isNum(whole[i]):
+			b = append(b, whole[i])
 			i++
-			if words[i+1] == "struct" {
-				S.name = words[i]
-				i += 3
-				for words[i] != "}" {
-					S.vals = append(S.vals, []string{words[i], words[i+1]})
-					i += 2
-				}
-				i += 3
-			}
-			S.iface = words[i]
-			i += 2
-			for words[i] != "}" {
-				splits := strings.Split(words[i], "(")
-				name := splits[0]
-				fmt.Println(name)
-				if len(splits) > 1 {
-					if splits[1] == ")" {
-						i++
-					} else {
-						splitten := strings.Split(splits[1], ",")
-						fmt.Println(splitten)
-						fmt.Println(splits[1])
-					}
-				}
-				fmt.Println(words[i])
+			for isNum(whole[i]) || whole[i] == '.' || whole[i] == ',' {
+				b = append(b, whole[i])
 				i++
 			}
+			lexed = append(lexed, b)
+		case isSymbol(whole[i]):
+			lexed = append(lexed, []byte{whole[i]})
+			i++
 		}
-		break
+		if i >= len(whole) {
+			break
+		}
 	}
-	fmt.Println(S)
+	for i := range lexed {
+		if string(lexed[i]) == "package" {
+			i++
+			fmt.Println("package", string(lexed[i]))
+		}
+		if string(lexed[i]) == `"` {
+			fmt.Println("Open quotes")
+			a := lexed[i]
+			i++
+			for string(lexed[i]) != `"` {
+				a = append(a, lexed[i]...)
+				i++
+			}
+			a = append(a, lexed[i]...)
+		}
+	}
 }
