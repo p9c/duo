766d5a85ac89bad2eed64f9a9b15ca37a7558158
Loki Verloren <l0k1@null.net>
2018-10-09 19:52:56 +0200

    got legacy docker fully working using shared host network

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 Work in progress...
 
 v0.0.0 is not a release but there is lots inside it, and lots more coming!
+
+## Parallelcoin docker inside!
+
+In the `docker/legacy` directory, if you are running linux and have docker installed, if you run `source init.sh` and `halp` you will get instructions how to get the legacy parallelcoind running in a docker (for windows/mac you will need to control it differently, possibly mac can use the aliases, on windows maybe a mingw shell). Basically, to make it run, you can do a one liner:
+
+    .stop;.rm;.build;.run;.start
+
+and it will be listening on 127.0.0.1:11048 for RPC and 0.0.0.0:11047 for p2p communication.
--- docker/legacy/init.sh
+++ docker/legacy/init.sh
@@ -14,15 +14,15 @@ alias   .where="echo $DATADIR"
          ### [ show where the current instance activated by init.sh lives ]
 alias      .cd="cd $DATADIR"
          ### [ change working directory to instance folder ]
-alias     .run="sudo docker run -v $DATADIR/data:/work -d=true --name $NAME $NAME"
+alias     .run="sudo docker run --network=\"host\" -v $DATADIR/data:/root/.parallelcoin -d=true -p 11047:11047 -p 11048:11048 -p 21047:21047 -p 21048:21048 --name $NAME $NAME"
          ### [ start up the container (after building, to restart. for a'.stop'ed container, use '.start') ]
 alias   .start="sudo docker start $NAME"
          ### [ start the container that was previously '.stop'ed ]
 alias    .stop="sudo docker stop $NAME"
          ### [ stop the container, start it again with '.start' ]
-alias   .enter="sudo docker exec -it $NAME zsh"
+alias   .enter="sudo docker exec -it $NAME bash"
          ### [ open a shell inside the container ]
-alias     .log="sudo tail -f $DATADIR/data/steemd.log"
+# alias     .log="sudo tail -f $DATADIR/data/steemd.log"
          ### [ show the current output from the primary process in the container ]
 alias   .build="sudo docker build -t $NAME $DATADIR"
          ### [ build the container from the Dockerfile ]
