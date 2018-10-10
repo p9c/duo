111f813529f177a2ff0072842d615e04b4dd68e2
Loki Verloren <l0k1@null.net>
2018-10-10 08:10:11 +0200

    now does not run if it finds blockchain file

--- cmd/chainsync/main.go
+++ cmd/chainsync/main.go
@@ -36,83 +36,85 @@ func main() {
 		os.Exit(1)
 	}
 
-	blockchain, err := os.Create(path + "/" + db.DefaultBaseDir + "/blocks")
-	if err != nil {
-		fmt.Println("creating blockchain file", err.Error())
-		os.Exit(1)
-	}
-	defer blockchain.Close()
+	blockstoreBaseDir := path + "/" + db.DefaultBaseDir
 
-	err = fallocate.Fallocate(blockchain, 0, 256*256*256*256)
-	if err != nil {
-		fmt.Println("allocating blockchain file", err.Error())
-		os.Exit(1)
-	}
+	if _, err := os.Stat(blockstoreBaseDir + "/blocks"); !os.IsNotExist(err) {
 
-	dbOptions := &badger.DefaultOptions
-	dbOptions.Dir = path + "/" + db.DefaultBaseDir + "/blockchain"
-	dbOptions.ValueDir = dbOptions.Dir + "/values"
-	db, err := badger.Open(*dbOptions)
-	if err != nil {
-		fmt.Println("opening db", err.Error())
-		os.Exit(1)
-	}
-	defer db.Close()
+	} else {
 
-	var hashes [][]byte
-	var position uint64
-
-	for i := uint64(0); i < str.Blocks; i++ {
-		getHash, err := C.Call("getblockhash", []uint64{i})
+		blockchain, err := os.Create(blockstoreBaseDir + "/blocks")
 		if err != nil {
-			hashes = append(hashes, []byte{})
-		} else {
-			unquoted, _ := strconv.Unquote(string(getHash.Result))
-			bytes, _ := hex.DecodeString(string(unquoted))
-			hashes = append(hashes, bytes)
-			resp, _ = C.Call("getblock", []interface{}{hex.EncodeToString(hashes[i]), false})
-			unquoted, _ = strconv.Unquote(string(resp.Result))
-			bytes, _ = hex.DecodeString(unquoted)
-			start := position
-			length := uint32(len(bytes))
-			position += uint64(length)
-			blockchain.Write(bytes)
-
-			// fmt.Println("k 01", fmt.Sprintf("%06x", i), "v", hex.EncodeToString(hashes[i]))
-			// fmt.Println("k 02", hex.EncodeToString(hashes[i]), "v", fmt.Sprintf("%08x", i))
-			// fmt.Println("k 04", fmt.Sprintf("%06x v %08x %08x", i, start, end))
-			// fmt.Println()
-
-			k1 := *core.IntToBytes(0x01000000 & i)
-			v1 := hashes[i]
-			k2 := append([]byte{2}, hashes[i]...)
-			v2 := *core.IntToBytes(i)
-			k3 := *core.IntToBytes(0x04000000 & i)
-			v3 := append(*core.IntToBytes(start), *core.IntToBytes(length)...)
-			err := db.Update(func(txn *badger.Txn) error {
-				return txn.Set(k1, v1)
-			})
-			if err != nil {
-				fmt.Println("writing kv 1", err.Error())
-				os.Exit(1)
-			}
-			err = db.Update(func(txn *badger.Txn) error {
-				return txn.Set(k2, v2)
-			})
-			if err != nil {
-				fmt.Println("writing kv 2", err.Error())
-				os.Exit(1)
-			}
-			err = db.Update(func(txn *badger.Txn) error {
-				return txn.Set(k3, v3)
-			})
-			if err != nil {
-				fmt.Println("writing kv 3", err.Error())
-				os.Exit(1)
-			}
+			fmt.Println("creating blockchain file", err.Error())
+			os.Exit(1)
 		}
-		if i%1000 == 0 {
-			fmt.Print(i, "...")
+		defer blockchain.Close()
+
+		err = fallocate.Fallocate(blockchain, 0, 256*256*256*256)
+		if err != nil {
+			fmt.Println("allocating blockchain file", err.Error())
+			os.Exit(1)
+		}
+
+		dbOptions := &badger.DefaultOptions
+		dbOptions.Dir = blockstoreBaseDir + "/blockchain"
+		dbOptions.ValueDir = dbOptions.Dir + "/values"
+		db, err := badger.Open(*dbOptions)
+		if err != nil {
+			fmt.Println("opening db", err.Error())
+			os.Exit(1)
+		}
+		defer db.Close()
+
+		var hashes [][]byte
+		var position uint64
+
+		for i := uint64(0); i < str.Blocks; i++ {
+			getHash, err := C.Call("getblockhash", []uint64{i})
+			if err != nil {
+				hashes = append(hashes, []byte{})
+			} else {
+				unquoted, _ := strconv.Unquote(string(getHash.Result))
+				bytes, _ := hex.DecodeString(string(unquoted))
+				hashes = append(hashes, bytes)
+				resp, _ = C.Call("getblock", []interface{}{hex.EncodeToString(hashes[i]), false})
+				unquoted, _ = strconv.Unquote(string(resp.Result))
+				bytes, _ = hex.DecodeString(unquoted)
+				start := position
+				length := uint32(len(bytes))
+				position += uint64(length)
+				blockchain.Write(bytes)
+
+				k1 := *core.IntToBytes(0x01000000 & i)
+				v1 := hashes[i]
+				k2 := append([]byte{2}, hashes[i]...)
+				v2 := *core.IntToBytes(i)
+				k3 := *core.IntToBytes(0x04000000 & i)
+				v3 := append(*core.IntToBytes(start), *core.IntToBytes(length)...)
+				err := db.Update(func(txn *badger.Txn) error {
+					return txn.Set(k1, v1)
+				})
+				if err != nil {
+					fmt.Println("writing kv 1", err.Error())
+					os.Exit(1)
+				}
+				err = db.Update(func(txn *badger.Txn) error {
+					return txn.Set(k2, v2)
+				})
+				if err != nil {
+					fmt.Println("writing kv 2", err.Error())
+					os.Exit(1)
+				}
+				err = db.Update(func(txn *badger.Txn) error {
+					return txn.Set(k3, v3)
+				})
+				if err != nil {
+					fmt.Println("writing kv 3", err.Error())
+					os.Exit(1)
+				}
+			}
+			if i%1000 == 0 {
+				fmt.Print(i, "...")
+			}
 		}
 	}
 	fmt.Println()
