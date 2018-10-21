b1924235431d84e49c30012a035838e8361c2af2
Loki Verloren <l0k1@null.net>
2018-10-21 16:09:20 +0200

    minor change to varint test

--- pkg/sync/util_test.go
+++ pkg/sync/util_test.go
@@ -9,25 +9,24 @@ import (
 func TestVarints(t *testing.T) {
 	var bytes []byte
 	slice := []interface{}{
-		int(1),
-		uint(10),
+		int(5),
+		uint(13),
 		int8(100),
-		uint8(100),
-		int16(10000),
-		uint16(10000),
-		int32(100000000),
-		uint32(1000000000),
-		int64(1000000000),
-		uint64(1000000000),
+		uint8(140),
+		int16(10035),
+		uint16(10250),
+		int32(10020000),
+		uint32(1003230000),
+		int64(10000006932),
+		uint64(1002000302),
 	}
 	for j := range slice {
-		// fmt.Println(hex.EncodeToString(bytes))
 		bytes = AppendVarint(bytes, slice[j])
-		fmt.Println(hex.EncodeToString(bytes))
+		fmt.Println(slice[j], hex.EncodeToString(bytes))
 	}
 	for i := range slice {
 		var outint interface{}
 		bytes, outint = ExtractVarint(slice[i], bytes)
-		fmt.Println(outint, ", ", hex.EncodeToString(bytes))
+		fmt.Println(outint, hex.EncodeToString(bytes))
 	}
 }
